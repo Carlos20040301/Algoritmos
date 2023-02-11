4dc29e5e99d471f96daaf037b6ba1853a96b46a5
Carlos20040301 <80127423+Carlos20040301@users.noreply.github.com>
2023-02-10 21:41:14 -0600

    cap2

---
+++ Cap_2/ArrayClient ( 2-1).py
@@ -0,0 +1,18 @@
+import Array
+maxSize = 10 
+arr = Array.Array(maxSize) 
+arr.insert(77) 
+arr.insert(44)
+arr.insert(5)
+arr.insert(9)
+arr.insert(44)
+arr.insert(555)
+arr.insert(12.34)
+arr.insert(0)
+arr.insert(2)
+arr.insert(-17)
+
+m=arr.getMaxNum()
+arr.traverse()
+print("El máximo es:", m)
+
---
+++ Cap_2/ArrayClient ( 2-2).py
@@ -0,0 +1,22 @@
+import Array
+maxSize = 10 
+arr = Array.Array(maxSize) 
+arr.insert(77) 
+arr.insert(44)
+arr.insert(5)
+arr.insert(9)
+arr.insert(44)
+arr.insert(555)
+arr.insert(12.34)
+arr.insert(0)
+arr.insert(2)
+arr.insert(-17)
+arr.traverse()
+m=arr.getMaxNum()
+print ("bip bop eliminando a:",m)
+
+arr.deleteMaxNum()
+
+print("comprobando...")
+
+arr.traverse()
