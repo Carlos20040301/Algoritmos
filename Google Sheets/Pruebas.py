5e5e4ff546fc8ddeed3f56defc74ecdf98d225c1
Carlos20040301 <80127423+Carlos20040301@users.noreply.github.com>
2023-04-12 21:05:49 -0600

    gugol

---
+++ Google Sheets/Pruebas.py
@@ -0,0 +1,15 @@
+import gspread
+import random
+import pygsheet
+import array
+
+arr = []
+
+for i in range(10):
+     z = random.randrange(1,10)
+     arr.append(z)
+
+print(arr)
+
+for i in range (10):
+    print(arr[i])
